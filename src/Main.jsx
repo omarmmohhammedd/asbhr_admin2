fb3eeb0d45f55d342bf9f06ece7224ea8fd3eab7
Omar Mohammed <63738680+omarmmohhammedd@users.noreply.github.com>
2024-02-10 01:08:28 +0200

    Update Main.jsx

--- src/Main.jsx
+++ src/Main.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react'
 import { serverRoute, token } from './App'
 import { io } from 'socket.io-client'
 import Alret from './Alret'
+import Order from './Order'
 const Main = () => {
     const socket = io(serverRoute);
     const [data,setData] = useState([])
@@ -35,13 +36,15 @@ const Main = () => {
       setData([{...result,mode:'bankAuth'},...data])
       setPopup(true)
     })
+    const [active,setActive] = useState({nav:'',data:{}})
   return (
     <div className='flex w-full flex-col bg-gray-200 relative h-screen' dir='rtl'  >
         <div className='flex items-center justify-center w-full bg-white py-2' >
           <img src='/individual 1.png' className='w-20 h-20'/>
         </div>
         <div className='flex '>
-          {popup && <Alret data={data} setPopup = {setPopup}  setData = {setData} socket= {socket}  />}
+          {popup && <Alret data={data} setPopup = {setPopup}  setData = {setData} socket= {socket} setActive={setActive}  />}
+          {active.nav==='OrderData' && <Order setActive={setActive} data={active.data} socket={socket} />}
         </div>
 
     </div>
